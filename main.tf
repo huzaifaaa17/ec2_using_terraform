69abcaedc446de00fd1278cd4174f021542183d5
Muhammad Huzaifa <huzaifarajpoot006@gmail.com>
2026-08-22 15:21:33 +0500

    used modules/functions to create an ec2 machine

--- main.tf
+++ main.tf
@@ -1,18 +1,50 @@
+# # provider "aws" {
+# #     region = "eu-north-1"
+# # }
+
+# # resource "aws_instance" "my_ec2" {
+# #     ami = "ami-0734cbe7f841a2e9b"
+# #     instance_type = "t3.micro"
+# #     tags = {
+# #         Name = "MyFirstTerraformEC2"
+# #     }
+# # }
+
+# #variables
 # provider "aws" {
-#     region = "eu-north-1"
+#     region = var.aws_region
 # }
 
-# resource "aws_instance" "my_ec2" {
-#     ami = "ami-0734cbe7f841a2e9b"
-#     instance_type = "t3.micro"
-#     tags = {
-#         Name = "MyFirstTerraformEC2"
+# data "aws_ami" "amazon_linux" {
+#     most_recent = true
+#     owners = ["amazon"]
+
+#     filter {
+#         name = "name"
+#         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
 #     }
 # }
 
-#variables
+# resource "aws_s3_bucket" "hayze_bucket" {
+#     bucket = var.bucket_name
+#     tags = {
+#         Name = "MyS3Bucket"
+#     }
+# }
+
+# resource "aws_instance" "my_ec2" {
+#     ami = data.aws_ami.amazon_linux.id
+#     instance_type = var.instance_type
+#     tags = {
+#         Name = var.instance_name
+#     }
+#     depends_on = [ aws_s3_bucket.hayze_bucket ]
+# }
+
+#MODULES
+
 provider "aws" {
-    region = var.aws_region
+    region = "eu-north-1"
 }
 
 data "aws_ami" "amazon_linux" {
@@ -25,18 +57,9 @@ data "aws_ami" "amazon_linux" {
     }
 }
 
-resource "aws_s3_bucket" "hayze_bucket" {
-    bucket = var.bucket_name
-    tags = {
-        Name = "MyS3Bucket"
-    }
-}
-
-resource "aws_instance" "my_ec2" {
+module "ec2_instance" {
+    source = "./ec2-modules"
     ami = data.aws_ami.amazon_linux.id
-    instance_type = var.instance_type
-    tags = {
-        Name = var.instance_name
-    }
-    depends_on = [ aws_s3_bucket.hayze_bucket ]
+    instance_type = "t3.micro"
+    name = "MyModuleEC2"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
- output "bucket_name" {
-    value = aws_s3_bucket.hayze_bucket.bucket
- }
+#  output "bucket_name" {
+#     value = aws_s3_bucket.hayze_bucket.bucket
+#  }
