8daa798bb8d4836695820c8cb128cbb98eeb1b94
Muhammad Huzaifa <huzaifarajpoot006@gmail.com>
2026-08-22 16:11:35 +0500

    osed workspaces to build different ec2 machines dev , stage

--- main.tf
+++ main.tf
@@ -43,8 +43,31 @@
 
 #MODULES
 
+# provider "aws" {
+#     region = "eu-north-1"
+# }
+
+# data "aws_ami" "amazon_linux" {
+#     most_recent = true
+#     owners = ["amazon"]
+
+#     filter {
+#         name = "name"
+#         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#     }
+# }
+
+# module "ec2_instance" {
+#     source = "./ec2-modules"
+#     ami = data.aws_ami.amazon_linux.id
+#     instance_type = "t3.micro"
+#     name = "MyModuleEC2"
+# }
+
+#WORKSPACE
+
 provider "aws" {
-    region = "eu-north-1"
+    region = var.aws_region
 }
 
 data "aws_ami" "amazon_linux" {
@@ -57,9 +80,11 @@ data "aws_ami" "amazon_linux" {
     }
 }
 
-module "ec2_instance" {
-    source = "./ec2-modules"
+resource "aws_instance" "my_ec2" {
     ami = data.aws_ami.amazon_linux.id
-    instance_type = "t3.micro"
-    name = "MyModuleEC2"
+    instance_type = var.instance_type
+    tags = {
+        Name = "EC2-${terraform.workspace}"
+        Environment = terraform.workspace
+    }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 #  output "bucket_name" {
 #     value = aws_s3_bucket.hayze_bucket.bucket
 #  }
+
+#WORKSPACES
+
+output "workspace_name" {
+    value = terraform.workspace
+}
